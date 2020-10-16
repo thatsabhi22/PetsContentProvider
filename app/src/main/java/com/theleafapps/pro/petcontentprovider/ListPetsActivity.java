88721ede8d5ec46eb5d370fdab192efe966a6200
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-10-16 17:51:21 +0530

    Implemented CursorAdapter and query logic

--- app/src/main/java/com/theleafapps/pro/petcontentprovider/ListPetsActivity.java
+++ app/src/main/java/com/theleafapps/pro/petcontentprovider/ListPetsActivity.java
@@ -7,10 +7,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListPetsActivity extends AppCompatActivity {
+
+    /** Identifier for the pet data loader */
+    private static final int PET_LOADER = 0;
+
+    /** Adapter for the ListView */
+    PetCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,19 @@ public class ListPetsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new PetCursorAdapter(this, null);
+        petListView.setAdapter(mCursorAdapter);
+
     }
 
     @Override
